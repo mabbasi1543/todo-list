aee4bdd67302595a3e233bc43ebb5d56f9dbe560
mabbasi1543 <m.abbasi1543@gmail.com>
2022-09-12 02:36:50 +0430

    added actions using id

--- src/utility.js
+++ src/utility.js
@@ -1,19 +1,44 @@
-const createElement = (parent, tag, text = "", classes) => {
-    const temp = document.createElement(tag);
-    if (classes != undefined) {
-        const classArr = classes.split(" ");
-        if (classArr.length > 0) {
-            classArr.forEach((item) => temp.classList.add(item))
+const DOMUtility = (() => {
+    const createElement = (parent, tag, text = "", classes, id) => {
+        const temp = document.createElement(tag);
+        if (id != undefined) {
+            temp.id = id;
+        }
+        if (classes != undefined && classes != "") {
+            const classArr = classes.split(" ");
+            if (classArr.length > 0) {
+                classArr.forEach((item) => temp.classList.add(item))
+            }
+        }
+        if (text == "") {
+            parent.appendChild(temp)
+        } else {
+            temp.innerText = text
+            parent.appendChild(temp)
+        }
+        return (temp)
+    }
+    const editById = (id) => {
+        return document.getElementById(id)
+    }
+
+    const checkById = (id) => {
+        let element = document.getElementById(id)
+        if (element != 'undefined' && element != null) {
+            return false;
+        } else {
+            return true;
         }
     }
-    if (text == "") {
-        parent.appendChild(temp)
-    } else {
-        temp.innerText = text
-        parent.appendChild(temp)
+    const selectById = (id) => {
+        return document.getElementById(id);
+
     }
-    return (temp)
-}
-
-
-export default createElement;
+    return {
+        createElement,
+        editById,
+        checkById,
+        selectById,
+    }
+})()
+export default DOMUtility;
