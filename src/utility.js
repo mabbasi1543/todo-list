2a1c988dd0b3ff51ebc5254039bb68cfe6038cda
mabbasi1543 <m.abbasi1543@gmail.com>
2022-09-11 17:06:07 +0430

    added empty classes check

--- src/utility.js
+++ src/utility.js
@@ -1,8 +1,10 @@
 const createElement = (parent, tag, text = "", classes) => {
     const temp = document.createElement(tag);
-    const classArr = classes.split(" ");
-    if (classArr.length > 0) {
-        classArr.forEach((item) => temp.classList.add(item))
+    if (classes != undefined) {
+        const classArr = classes.split(" ");
+        if (classArr.length > 0) {
+            classArr.forEach((item) => temp.classList.add(item))
+        }
     }
     if (text == "") {
         parent.appendChild(temp)
